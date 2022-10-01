315370de4c48639a2ffe838484a744f6dc9b6cec
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2022-10-01 15:14:40 +0530

    add: linear regression empty source file (#10)

---
+++ algorithms/linear_regression.go
@@ -0,0 +1,63 @@
+package main
+// import whatever packages you will require here
+
+// use the following sample dataset
+/*
+| x     | y     |
+|-------|-------|
+| 4512  | 1530  |
+| 3738  | 1297  |
+| 4261  | 1335  |
+| 3777  | 1282  |
+| 4177  | 1590  |
+*/
+
+// expect the dataset to be in csv form
+
+// FUNCTION DEFINITION
+// add parameters and change the return type as necessary
+
+
+// function to calculate the mean of x and y
+
+func mean() float32 {
+  // insert code here
+}
+
+// function to calculate slope m and intercept c using the required formula
+
+// // FORMULA
+// // m = (Σ(x - x')(y - y'))/(Σ(x-x')^2)
+// // where x' and y' are the means of x and y respectively
+
+func calculate_slope_and_intercept() (float32, float32) {
+  // insert code here
+}
+
+// function to plot regression line
+
+func plot_regression_line() {
+  // insert code here
+}
+
+// function to plot the data points
+
+func plot_data_points() {
+  // insert code here
+}
+
+// function to calculate R-squared value to measure accuracy of model
+
+// // FORMULA
+// R^2 = (Σ(y_pred - y')^2)/(Σ(y-y')^2)
+// where y_pred is the predicted value for y, y' is the mean
+
+func calculate_r_squared() float32 {
+  // insert code here
+}
+
+// MAIN FUNCTION
+
+func main() {
+
+}
