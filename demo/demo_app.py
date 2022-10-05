9758f301a37ba4e03ef7b5c8f780cd5dbb3c929c
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2022-10-05 20:16:35 +0530

    feat: added makefile

--- demo/demo_app.py
+++ demo/demo_app.py
@@ -2,7 +2,7 @@
 import ctypes
 
 # load the shared library into c types
-library = ctypes.cdll.LoadLibrary('./library.so')
+library = ctypes.cdll.LoadLibrary('./build/library.so')
 
 # call the shared library function
 hello_world = library.helloWorld
