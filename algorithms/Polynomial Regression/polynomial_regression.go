18805cd4a8b3224bddbb721d0c5643ee957ba9ff
B.Hariprasad <75137153+Hariprasad19036@users.noreply.github.com>
2022-10-09 17:20:58 +0530

    Polynomial Regression Template (#29)
    * Polynomial Regression Template
    
    * Added formula in comments

---
+++ algorithms/Polynomial Regression/polynomial_regression.go
@@ -0,0 +1,78 @@
+package main
+
+// import whatever packages you will require here
+
+// use the following sample dataset
+/*
+| x     | y     |
+|-------|-------|
+| 4512  | 1530  |
+| 3738  | 1297  |
+| 4261  | 1335  |
+| 3777  | 1282  |
+| 4177  | 1590  |
+*/
+
+// expect the dataset to be in csv form
+
+// FUNCTION DEFINITION
+// add parameters and change the return type as necessary
+
+// function to read csv file (dataset)
+
+func read_csv(filename string) ([][]float32, error) {
+
+}
+
+// function to calculate weights
+
+// // FORMULA
+// W = Inverse(X' . X) . X' . y
+// where X' is the transpose of X and . denotes dot product
+
+func calc_weights() {
+
+}
+
+// function to predict y values
+
+// // FORMULA
+// y = X . W
+// Where W is the calculated weights and . denotes dot product
+
+func predict_y() {
+
+}
+
+// function to calculate mean
+func mean() {
+
+}
+
+// function to plot regression line
+
+func plot_regression_line() {
+	// insert code here
+}
+
+// function to plot the data points
+
+func plot_data_points() {
+	// insert code here
+}
+
+// function to calculate R-squared value to measure accuracy of model
+
+// // FORMULA
+// R^2 = (Σ(y_pred - y')^2)/(Σ(y-y')^2)
+// where y_pred is the predicted value for y, y' is the mean
+
+func calculate_r_squared() float32 {
+	// insert code here
+}
+
+// MAIN FUNCTION
+
+func main() {
+
+}
